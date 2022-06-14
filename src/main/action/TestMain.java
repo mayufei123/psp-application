5dbc2299c53cdf6c5ad1a80333cdc42b8ddd43e5
mayufei123 <617006311@qq.com>
2022-06-14 13:26:08 +0800

    创建项目

---
+++ src/main/action/TestMain.java
@@ -0,0 +1,14 @@
+package action;
+
+import entity.Student;
+
+public class TestMain {
+    public static void main(String[] args) {
+        System.out.println("===========This is Action=========");
+        Student student = new Student();
+        student.setName("小明");
+        student.setAge(3);
+        System.out.println("StudentInfo:"+student.getName()+";"+student.getAge()+";");
+
+    }
+}
