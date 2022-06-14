5087f3f92224eb708f4cf632add012465a19f645
mayufei123 <617006311@qq.com>
2022-06-14 13:59:13 +0800

    新建项目3

--- src/main/java/com/test/HelloWorld.java
+++ src/main/java/com/test/HelloWorld.java
@@ -2,7 +2,7 @@ package com.test;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("=========Hello-==============");
+        System.out.println("===========Hello-==============");
 
     }
 }
